96d6687e2149baab83384839ee035f90c7b016b5
mj <mjrodriguez.uz@gmail.com>
2023-01-16 19:48:32 +0100

    primer commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import {pool} from './db.js'
+import {PORT} from './config.js'
+
+//Creacion de la app web con express
+const app = express()
+
+// Puerto de la aplicación web
+app.listen(PORT)
+console.log('servidor en el puerto 3000')
+
+app.get('/',async(req, res) =>{
+	// res.send("IAW")
+	const [result] = await pool.query(`SELECT * from userdb.users`)
+	res.json(result)
+})
+app.get('/ping',async(req, res) =>{
+	const result = await pool.query(`SELECT "hola maria" AS RESULT`)
+	console.log(result[0])
+})
+app.get('/create',async(req, res) =>{
+	const insertar = await pool.query(`INSERT into users (id, name) VALUES (' ', 'pablo')`)
+	console.log(insertar[0])
+})
+
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT || 3000 //La 2ble barra me dice que por defecto 
+												//coja el puerto 3000 en caso de que el puerto que de el servidor no sirva
+
+
